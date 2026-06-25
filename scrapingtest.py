98ab92786dbcec18a01de3c651d8ec0f1a7cb3c0
WoodGoblin <pasynkov.aleksey@gmail.com>
2026-06-25 16:20:16 +0300

    Spider scraper for Beer.rf and Grainrus.ru
    * universal spider with url clearing for cyrillic sites
    * parsers module, incl:
      - parsing config
      - content controller (updating only price for existing entries)
      - specific parsing classes for each site
      - output as json file

--- scrapingtest.py
+++ scrapingtest.py
@@ -1,27 +1,9 @@
 import logging
 
-#from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urlunparse, unquote
 import scrapy
 from scrapy.crawler import CrawlerProcess
-
-def parse_beer_rf(response):
-    print("parsing beer.rf")
-
-def parse_grainrus(response):
-    print("parsing grainrus")
-
-PARSING_CONFIG = {
-    'beer_rf':{
-            'start_urls': ["https://бир.рф/shop/grain/malt/"],
-            'parse_function': parse_beer_rf
-    },
-    'grainrus':{
-            'start_urls': ["https://grainrus.com/"],
-            'parse_function': parse_grainrus
-    }
-}
-
+from parsers import PARSING_CONFIG, ContentController
 
 class CyrillicUrlMiddleware:
     def _decode_url(self, url):
@@ -50,6 +32,7 @@ class Scraper(scrapy.Spider):
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         'FEED_EXPORT_ENCODING': 'utf-8',
+        'FEEDS': { 'outputs/products_data.json': {'format': 'json', 'encoding': 'utf8', 'overwrite': True} },
         'COOKIES_ENABLED': True,
         'DOWNLOADER_MIDDLEWARES': { '__main__.CyrillicUrlMiddleware': 543, },
         'DOWNLOAD_DELAY': 1.5, 
@@ -59,17 +42,15 @@ class Scraper(scrapy.Spider):
     async def start(self):
         print("starting request")
         sites = ['beer_rf', 'grainrus']
-        start_urls = ["https://бир.рф/shop/grain/malt/",
-                    "https://pikabu.ru/",
-                    "https://grainrus.com/"
-                    ]
         
-        for site in sites:
-            if site not in PARSING_CONFIG:
-                raise ValueError(f"Site '{site}' is not defined in PARSING_CONFIG")
-            for url in PARSING_CONFIG[site]['start_urls']:
-                yield scrapy.Request(url=url, callback=PARSING_CONFIG[site]['parse_function'])
+        content_controller = ContentController()
 
+        for product, config in PARSING_CONFIG.items():
+            for site in config.values():
+                parser = site['parser_class'](content_controller)
+                for url in site['start_urls']:
+                    yield scrapy.Request(url=url, callback=parser.parse, cb_kwargs={'item': product()})
+        
 
 
 def main():
