77f8cab8c1a647e482c5669ff8070a54f3ad7cdf
WoodGoblin <pasynkov.aleksey@gmail.com>
2026-07-08 21:40:30 +0300

    Correction: fixture in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,9 @@ import pandas as pd
 import json
 from beeromancy_back.data_processers import ScrapedDataProcesser
 
-@pytest.fixture
-def test_raw_data(scope="session"):
+@pytest.fixture(scope="session")
+def test_raw_data():
     path = Path(__file__).parent / "test_data"/"test_products_data.json"
     processer = ScrapedDataProcesser()
     with open(path, 'r', encoding='utf-8') as f:
-        # return pd.json_normalize(json.load(f))
         return processer.get_cleared_data(f)
