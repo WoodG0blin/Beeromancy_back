82cada7c9384d7888585078539cc25de8fcb098f
WoodGoblin <pasynkov.aleksey@gmail.com>
2026-07-23 21:14:12 +0300

    Database base schema and setup
    * SQLAlchemy setup with ORM
    * fill of base DIM tables, filter by new elements with hashing

---
+++ beeromancy_back/db_connect.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from sqlalchemy import ForeignKey, select, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+import db_schema as schema
+from data_processers import PRODUCER_BASE_COUNTRIES
+
+class DatabaseController():
+    def __init__(self):
+        self.engine = create_engine("sqlite:///beeromancy.db", echo=False)
+        schema.Base.metadata.create_all(bind=self.engine)
+
+
+    def __enter__(self):
+        self.session = Session(self.engine)
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if hasattr(self, 'session') and self.session:
+            if exc_type is not None:
+                self.session.rollback()  # Откатываем, если была ошибка
+                print(f"[DB] Транзакция откатана из-за ошибки: {exc_val}")
+            else:
+                self.session.commit()    # Сохраняем, если всё прошло гладко
+            self.session.close()
+
+    def get_ingredients_mapping(self) -> pd.DataFrame:
+        q = (
+            select(schema.IngredientMapping.parser_name.label("clean_name"), schema.Ingredient.name.label("master_name"), schema.Producer.name.label("brand"))
+            .select_from(schema.IngredientMapping).join(schema.Ingredient).join(schema.Producer)
+        )
+
+        return pd.read_sql_query(q, con = self.session.bind)
+
+    def load_ingredients_from_df(self, ingredients: pd.DataFrame, mapping: pd.DataFrame):
+        producer_cache = {p.name: p for p in self.session.scalars(select(schema.Producer)).all()}
+        country_cache = {c.country_code: c for c in self.session.scalars(select(schema.Country)).all()}
+        type_cache = {t.name: t for t in self.session.scalars(select(schema.IngrType)).all()}
+
+        category = ingredients.iloc[0]['item_type']
+        if category not in type_cache:
+            type_cache[category] = schema.IngrType(name = category)
+        ingr_type = type_cache[category]
+
+        chars_classes = {
+            'malt': schema.Malt_Characteristics,
+            'yeast': schema.Yeast_Characteristics,
+            'hop': schema.Hop_Characteristics
+        }
+        chars_class = chars_classes.get(category, None)
+
+        for _, ingr in ingredients.iterrows():
+            c_name = ingr['country']
+            if c_name not in country_cache:
+                country_cache[c_name] = schema.Country(country_code = c_name, name = c_name)
+            country = country_cache[c_name]
+            
+            prod_name = ingr['brand']
+            if prod_name not in producer_cache:
+                base_country = PRODUCER_BASE_COUNTRIES.get(prod_name)
+                if base_country and base_country not in country_cache:
+                    country_cache[base_country] = schema.Country(country_code = base_country, name = base_country)
+                producer_cache[prod_name] = schema.Producer(name = prod_name, base_country=country_cache.get(base_country))
+            producer = producer_cache[prod_name]
+
+            ingr_orm = schema.Ingredient(
+                name=ingr['clean_name'],
+                short_descr=ingr['description'],
+                full_descr= ingr['full_description']
+            )
+
+            ingr_orm.category = ingr_type
+            ingr_orm.producer = producer
+            ingr_orm.country = country
+
+            valid_cols = chars_class.__table__.columns.keys()
+            args = {k: v for k, v in ingr.to_dict().items() if k in valid_cols}
+            chars = chars_class(**args)
+            chars.ref_ingr = ingr_orm
+            self.session.add(chars)
+
+            current_mapping = mapping[(mapping['brand']==prod_name) & (mapping['master_name']==ingr['master_name'])].copy()
+            current_mapping.drop_duplicates(subset=['brand','clean_name'], inplace=True, keep=False)
+            for _, entry in current_mapping.iterrows():
+                m = schema.IngredientMapping(master_ingredient=ingr_orm, parser_name=entry['clean_name'])
+                # m.master_ingredient = ingr_orm
+            
+            self.session.add(ingr_orm)
---
+++ beeromancy_back/db_schema.py
@@ -0,0 +1,112 @@
+from sqlalchemy import ForeignKey, String, UniqueConstraint
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import Optional, List
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Country(Base):
+    __tablename__ = "dim_countries"
+
+    country_code: Mapped[str] = mapped_column(String(2), primary_key=True)
+    name: Mapped[str]
+
+class Producer(Base):
+    __tablename__ = "dim_producers"
+
+    producer_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    country_code: Mapped[Optional[str]] = mapped_column(ForeignKey("dim_countries.country_code"))
+
+    base_country: Mapped[Optional[Country]] = relationship()
+
+class IngrType(Base):
+    __tablename__ = "dim_ingr_types"
+
+    ingr_type: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+class Shop(Base):
+    __tablename__ = "dim_shops"
+
+    shop_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    start_url: Mapped[str]
+
+
+class Ingredient(Base):
+    __tablename__ = "dim_ingredients"
+
+    ingr_id: Mapped[int] = mapped_column(primary_key=True)
+    ingr_type = mapped_column(ForeignKey("dim_ingr_types.ingr_type"))
+    name: Mapped[str]
+    producer_id = mapped_column(ForeignKey("dim_producers.producer_id"))
+    country_code: Mapped[Optional[str]] = mapped_column(ForeignKey("dim_countries.country_code"))
+    short_descr: Mapped[str]
+    full_descr: Mapped[Optional[str]] = mapped_column(deferred = True)
+
+    category: Mapped[IngrType] = relationship()
+    producer: Mapped[Producer] = relationship()
+    country: Mapped[Country] = relationship()
+
+    parser_names: Mapped[List[IngredientMapping]] = relationship(back_populates="master_ingredient")
+
+class Malt_Characteristics(Base):
+    __tablename__ = "dim_malt_characteristics"
+
+    ingr_id = mapped_column(ForeignKey("dim_ingredients.ingr_id"), primary_key=True)
+    color_ebc_min: Mapped[Optional[float]]
+    color_ebc_max: Mapped[Optional[float]]
+    share_min: Mapped[Optional[float]]
+    share_max: Mapped[Optional[float]]
+    extract_min: Mapped[Optional[float]]
+    extract_max: Mapped[Optional[float]]
+    protein_min: Mapped[Optional[float]]
+    protein_max: Mapped[Optional[float]]
+    kolbach_min: Mapped[Optional[float]]
+    kolbach_max: Mapped[Optional[float]]
+    diastatic: Mapped[Optional[bool]] = mapped_column(insert_default=False)
+
+    ref_ingr: Mapped[Ingredient] = relationship()
+
+class Hop_Characteristics(Base):
+    __tablename__ = "dim_hop_characteristics"
+
+    ingr_id = mapped_column(ForeignKey("dim_ingredients.ingr_id"), primary_key=True)
+    alpha_min: Mapped[Optional[float]]
+    alpha_max: Mapped[Optional[float]]
+    beta_min: Mapped[Optional[float]]
+    beta_max: Mapped[Optional[float]]
+    cohumulon_min: Mapped[Optional[float]]
+    cohumulon_max: Mapped[Optional[float]]
+    oils_min: Mapped[Optional[float]]
+    oils_max: Mapped[Optional[float]]
+
+    ref_ingr: Mapped[Ingredient] = relationship()
+
+class Yeast_Characteristics(Base):
+    __tablename__ = "dim_yeast_characteristics"
+
+    ingr_id = mapped_column(ForeignKey("dim_ingredients.ingr_id"), primary_key=True)
+    attenuation_min: Mapped[Optional[float]]
+    attenuation_max: Mapped[Optional[float]]
+    flocculation: Mapped[Optional[str]]
+    ferment_temp_min: Mapped[Optional[float]]
+    ferment_temp_max: Mapped[Optional[float]]
+    alco_tolerance_min: Mapped[Optional[float]]
+    alco_tolerance_max: Mapped[Optional[float]]
+    diastatic: Mapped[Optional[str]] = mapped_column(insert_default=False)
+    fenolic: Mapped[Optional[str]] = mapped_column(insert_default=False)
+
+    ref_ingr: Mapped[Ingredient] = relationship()
+
+class IngredientMapping(Base):
+    __tablename__ = "dim_ingredient_mapping"
+    __table_args__ = (UniqueConstraint("ingr_id", "parser_name", name="uq_ingr_parser_name"),)
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ingr_id = mapped_column(ForeignKey("dim_ingredients.ingr_id"))
+    parser_name: Mapped[str] = mapped_column()
+
+    master_ingredient: Mapped[Ingredient] = relationship(back_populates="parser_names")
